cf7e7322795dc6c1b872fe1ca7a82ceba6a580ec
yerayyeray <ygutierrezm05@educantabria.es>
2025-12-01 22:24:35 +0100

    Delete Ejercicios Scripting directory

--- Ejercicios Scripting/Scripts en powershell/num-par.ps1
+++
@@ -1,9 +0,0 @@
-$numero = Read-Host "Introduce un número"
-
-$numero = [int]$numero
-
-if ($numero % 2 -eq 0) {
-    Write-Host "El número $numero es PAR."
-} else {
-    Write-Host "El número $numero es IMPAR."
-}
--- Ejercicios Scripting/Scripts en powershell/suma-numeros.ps1
+++
@@ -1,7 +0,0 @@
-$suma = 0
-
-for ($i = 1; $i -le 100; $i++) {
-    $suma += $i
-}
-
-Write-Host "La suma de los números del 1 al 100 es: $suma"
